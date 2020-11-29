e4d814291221dce76d7113a9c9ef5cae424cb632
Laurentiu Diaconu <dlorentsen1@gmail.com>
2020-11-29 20:02:32 +0200

    refactor to use reducers

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'change_counter':
+            return { ...state, counter: state.counter + action.payload }
+        default:
+            return state
+    } 
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    const { counter } = state
 
     return (
         <View>
-            <Button title="Increase Counter" onPress={() => {
-                setCounter(counter + 1);
-            }} />
-            <Button title="Decrease Counter" onPress={() => {
-                setCounter(counter - 1);
-            }} />
+            <Button title="Increase Counter" onPress={() => dispatch({ type: 'change_counter', payload: 1 }) } />
+            <Button title="Decrease Counter" onPress={() => dispatch({ type: 'change_counter', payload: -1}) } />
             <Text>Current Count: {counter}</Text>
         </View>
     );
